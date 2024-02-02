96cb71410d914467bc5710504c3a6eed1a960685
Colin Osborn <colin.osborn@gmail.com>
2024-02-02 09:15:57 -0700

    initial sketch out of game functions

---
+++ index.js
@@ -0,0 +1,26 @@
+let state = {};
+
+const canvas = document.getElementById('game');
+
+newGame();
+
+function newGame() {
+  // initialize game state
+  state = {}
+
+  draw();
+};
+
+function draw() {
+
+}
+
+//Event Handlers
+
+function throwBomb() {
+
+}
+
+function animate(timestamp) {
+  
+}
